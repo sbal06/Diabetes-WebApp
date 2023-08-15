eeb0d2fa0382de8e84f6997c5285dd10c8d8442c
sbal06 <101956177+sbal06@users.noreply.github.com>
2023-08-14 22:59:36 -0400

    Add files via upload

---
+++ DeployingMachineLearningModel/About.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import streamlit as st
+import pickle
+import os
+import xgboost
+import matplotlib.pyplot as plt
+from streamlit_extras.app_logo import add_logo
+#from streamlit_extras.no_default_selectbox import selectbox
+#from streamlit_extras.switch_page_button import switch_page
+
+
+
+
+st.set_page_config(layout="wide")
+
+
+def main():
+    st.title("About Page")
+    add_logo(r"C:\Users\shrey\Downloads\DeployingMachineLearningModel\diaaid.jpg", height = 100)
+  
+
+    st.header("This is Page2")
+    
+    
+            
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
+   #  st.write("Click here to access our research-based model")
+   #  if (st.button("Research-Based Model")):
+    #     switch_page('Research-Based Model')
+        
+        
+   #  st.write("Click here to access our machine-learning model")
+ 
