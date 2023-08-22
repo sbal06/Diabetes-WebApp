2721f279ca306743cf151aa60f7882afe4811d5c
sbal06 <101956177+sbal06@users.noreply.github.com>
2023-08-22 12:16:20 -0400

    Add files via upload

---
+++ DeployingMachineLearningModel/pages/3. Personalized Meal Plans.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 21 00:15:59 2023
+
+@author: Zhizhen L., Shreyes B.
+"""
+
+import streamlit as st
+import numpy as np
+
+st.header("Personalized Meal Plans")
+st.write("The development of the Personalized Meal Plans is in progress. Please be patient as we work towards its completion.")
+
+# st.write("If you know that you are diabetes, select very high risk.")
